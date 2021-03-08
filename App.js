01f433e7e9d80f5963442de4645929cf560487ef
punreachrany <punreach.rany@yahoo.com>
2021-03-08 13:11:46 +0900

    Nagivation Libraries Installed. Fonts setting error

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
+import * as Font from "expo-font";
+
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import React from "react";
-import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+      // <AppLoading startAsyc={fetchFonts} onFinish={() => setFontLoaded(true)} />
+    );
+  }
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Text font>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
